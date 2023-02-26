c49ea3a811e8f426ee2aaefaea80bee07f678efb
CrazyContraption <CrazyContraptionMC@gmail.com>
2023-02-25 19:01:13 -0500

    1.0 - Release

--- ReviveReaverShark.cs
+++ ReviveReaverShark.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
@@ -7,82 +10,140 @@ namespace ReviveReaverShark
 {
     public class ReviveReaverShark : Mod
     {
-        // Recalculate the powers of any of the specified player's items
+        /// <summary>
+        /// Forces a recalculation of a specific player's inventory of items, and updates the powers of them.
+        /// Also is responsible for informing the player of the changes to the item.
+        /// Server, menu, and worldgen calls are ignored.
+        /// </summary>
+        /// <param name="player">Player to search within and issue the updates to.</param>
         internal static void RecalculatePowers(Player player)
         {
+            if (Main.netMode is NetmodeID.Server || Main.gameMenu || WorldGen.gen)
+                return;
+
             if (player.active)
+            {
+                int oldMinPower = int.MaxValue;
+                int? newPower = GetPower();
+
                 foreach (var item in player.inventory)
-                    if (item.netID is ItemID.ReaverShark)
-                        RecalculatePowers(item);
+                    if (item.netID is ItemID.ReaverShark && item.active)
+                    {
+                        int oldpower = item.pick;
+                        int currPower = newPower ?? oldpower;
+                        item.pick = currPower;
+
+                        if (oldMinPower > oldpower)
+                            oldMinPower = oldpower;
+                    }
+                if (oldMinPower is int.MaxValue)
+                    return;
+
+                if (player.whoAmI == Main.myPlayer && newPower < oldMinPower)
+                    Main.NewText($"Your Reaver Shark shrivels with the lack of power!\nIt has been reduced to {newPower}% of its potential!", Color.Orange);
+                else if (player.whoAmI == Main.myPlayer && newPower > oldMinPower)
+                    Main.NewText($"Your Reaver Shark tembles with the power of your fallen foes!\nIt has reached {newPower}% of its potential!", Color.Orange);
+                
+            }
         }
 
-        // Recalculate the powers of a specific item
-        internal static bool RecalculatePowers(Item anItem)
+        /// <summary>
+        /// Gets the calculated power of the current world.
+        /// Server, menu, and worldgen calls are ignored.
+        /// </summary>
+        /// <returns>The pickaxe power of the world's closest progression.</returns>
+        internal static int? GetPower()
         {
-            if (anItem.active)
-            {
-                if (anItem is null)
-                    return false; // No item found
-
-                if (NPC.downedEmpressOfLight)
-                    anItem.pick = 300;
-                else if (NPC.downedMoonlord)
-                    anItem.pick = 250;
-                else if (NPC.downedTowerNebula || NPC.downedTowerSolar || NPC.downedTowerStardust || NPC.downedTowerVortex) // Any tower
-                    anItem.pick = 225;
-                else if (NPC.downedGolemBoss)
-                    anItem.pick = 210;
-                else if (NPC.downedMechBoss1 && NPC.downedMechBoss2 && NPC.downedMechBoss3) // All mechs
-                    anItem.pick = 200;
-                else if (NPC.downedMechBoss1 || NPC.downedMechBoss2 || NPC.downedMechBoss3) // Any mech
-                    anItem.pick = 170;
-                else if (NPC.downedQueenSlime)
-                    anItem.pick = 150;
-                else if (Main.hardMode) // WoF
-                    anItem.pick = 110;
-                else if (NPC.downedBoss2) // Either corruption boss
-                    anItem.pick = 70;
-                else if (NPC.downedBoss1) // EoC
-                    anItem.pick = 65;
-                // Item normally has 59% power
-
-                Main.NewText($"Your {anItem.Name}, tembles with the power of your fallen foes!\nIt has reached {anItem.pick}% of its potential!", Color.Orange);
-
-                return true; // Power update attempted
-            }
-            return false; // No player found
+            if (Main.netMode is NetmodeID.Server || Main.gameMenu || WorldGen.gen)
+                return null;
+            if (NPC.downedEmpressOfLight)
+                return 300;
+            else if (NPC.downedMoonlord)
+                return 250;
+            else if (NPC.downedTowerNebula && NPC.downedTowerSolar && NPC.downedTowerStardust && NPC.downedTowerVortex) // Any tower
+                return 225;
+            else if (NPC.downedTowerNebula || NPC.downedTowerSolar || NPC.downedTowerStardust || NPC.downedTowerVortex) // Any tower
+                return 225;
+            else if (NPC.downedAncientCultist)
+                return 220;
+            else if (NPC.downedGolemBoss)
+                return 210;
+            else if (NPC.downedPlantBoss)
+                return 205;
+            else if (NPC.downedMechBoss1 && NPC.downedMechBoss2 && NPC.downedMechBoss3) // All mechs
+                return 200;
+            else if (NPC.downedMechBoss1 || NPC.downedMechBoss2 || NPC.downedMechBoss3) // Any mech
+                return 170;
+            else if (NPC.downedQueenSlime)
+                return 150;
+            else if (Main.hardMode) // WoF
+                return 110;
+            else if (NPC.downedDeerclops) // Skeletron
+                return 100;
+            else if (NPC.downedBoss3) // Skeletron
+                return 90;
+            else if (NPC.downedQueenBee)
+                return 80;
+            else if (NPC.downedBoss2) // Either corruption boss
+                return 70;
+            else if (NPC.downedBoss1) // EoC
+                return 65;
+            else
+                return 59;
         }
     }
 
     public class ReaverItem : GlobalItem
     {
+        /// <summary>
+        /// Called whenever an item is created, copied, moved, or otherwise updated.
+        /// </summary>
+        /// <param name="item">The item being being set(up)</param>
         public override void SetDefaults(Item item)
         {
             base.SetDefaults(item);
             if (item.netID is ItemID.ReaverShark)
-                ReviveReaverShark.RecalculatePowers(item);
+                if (item.active)
+                    item.pick = ReviveReaverShark.GetPower() ?? item.pick;
         }
     }
 
     public class ReaverPlayer : ModPlayer
     {
+        /// <summary>
+        /// Runs when a player joins a world. Only called on the client that joined, and nobody else.
+        /// </summary>
+        /// <param name="player">The player that joined.</param>
         public override void OnEnterWorld(Player player)
         {
             base.OnEnterWorld(player);
-            ReviveReaverShark.RecalculatePowers(player);
+            new Task(delegate
+            {
+                Thread.Sleep(1000);
+                ReviveReaverShark.RecalculatePowers(Main.player[Main.myPlayer]);
+            }).Start();
         }
     }
 
     public class ReaverNPC : GlobalNPC
     {
+        /// <summary>
+        /// Called when an NPC is hit, just before the hit takes place.
+        /// </summary>
+        /// <param name="npc">The NPC being hit</param>
+        /// <param name="hitDirection">Direction of the hit</param>
+        /// <param name="damage">Damage of the hit</param>
         public override void HitEffect(NPC npc, int hitDirection, double damage)
         {
             base.HitEffect(npc, hitDirection, damage);
-            if (npc.boss)
-                if (npc.active)
-                    return;
-                else
-                    ReviveReaverShark.RecalculatePowers(Main.player[Main.myPlayer]);
+
+            if (npc.boss || npc.type is NPCID.LunarTowerVortex or NPCID.LunarTowerStardust or NPCID.LunarTowerSolar or NPCID.LunarTowerNebula) // Is the NPC tied to progression?
+                if (npc.life <= 0) // Is the boss dead/dying?
+                    new Task(delegate
+                    {
+                        Thread.Sleep(1000);
+                        ReviveReaverShark.RecalculatePowers(Main.player[Main.myPlayer]);
+                    }).Start();
         }
     }
 }
