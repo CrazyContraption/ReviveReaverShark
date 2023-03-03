42d513304e5422c11e0947c1130f168b74bafa5c
CrazyContraption <CrazyContraptionMC@gmail.com>
2023-03-03 11:31:10 -0500

    1.2 - Minor Additions

---
+++ ReaverConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader.Config;
+
+namespace ReviveReaverShark
+{
+    [Label("World Configs")]
+    public class ReaverConfig_Server : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Header("Server/World Settings")]
+
+        [Label("Use Dynamic Scaling")]
+        [Tooltip("When enabled, the Reaver Shark will dynamically scale in power according to progression.\n" +
+            "When disabled, the mod will simply un-nerf the Reaver Shark back to 100% flat power.")]
+        [DefaultValue(true)]
+        public bool dynamicScaling;
+
+        public override bool AcceptClientChanges(ModConfig pendingConfig, int whoAmI, ref string message)
+        {
+            if (pendingConfig is ReaverConfig_Server server)
+            {
+                if (server.dynamicScaling)
+                    message = "Dynamic Scaling enabled!";
+                else
+                    message = "Dynamic Scaling disabled!";
+                
+                return true;
+            }
+            return false;
+        }
+
+        public override void OnChanged()
+        {
+            if (Main.netMode is NetmodeID.Server || Main.gameMenu || WorldGen.gen || Main.PlayerLoaded is false)
+                return;
+
+            ReviveReaverShark.RecalculatePowers(Main.player[Main.myPlayer]);
+        }
+    }
+}
--- ReviveReaverShark.cs
+++ ReviveReaverShark.cs
@@ -10,6 +10,8 @@ namespace ReviveReaverShark
 {
     public class ReviveReaverShark : Mod
     {
+        internal static ReaverConfig_Server ServerConfig = ModContent.GetInstance<ReaverConfig_Server>();
+
         /// <summary>
         /// Forces a recalculation of a specific player's inventory of items, and updates the powers of them.
         /// Also is responsible for informing the player of the changes to the item.
@@ -18,13 +20,13 @@ namespace ReviveReaverShark
         /// <param name="player">Player to search within and issue the updates to.</param>
         internal static void RecalculatePowers(Player player)
         {
-            if (Main.netMode is NetmodeID.Server || Main.gameMenu || WorldGen.gen)
+            if (Main.netMode is NetmodeID.Server || Main.gameMenu || WorldGen.gen || Main.PlayerLoaded is false)
                 return;
-
+            
             if (player.active)
             {
                 int oldMinPower = int.MaxValue;
-                int? newPower = GetPower();
+                int? newPower = ServerConfig.dynamicScaling ? GetPower() : 100;
 
                 foreach (var item in player.inventory)
                     if (item.netID is ItemID.ReaverShark && item.active)
@@ -38,8 +40,9 @@ namespace ReviveReaverShark
                     }
                 if (oldMinPower is int.MaxValue)
                     return;
-
-                if (player.whoAmI == Main.myPlayer && newPower < oldMinPower)
+                if (player.whoAmI == Main.myPlayer && ServerConfig.dynamicScaling is false && newPower != oldMinPower)
+                    Main.NewText($"Your Reaver Shark feels a powerful worldly force preventing its true power. It's limited to {newPower}% of its potential!", Color.Orange);
+                else if (player.whoAmI == Main.myPlayer && newPower < oldMinPower)
                     Main.NewText($"Your Reaver Shark shrivels with the lack of power!\nIt has been reduced to {newPower}% of its potential!", Color.Orange);
                 else if (player.whoAmI == Main.myPlayer && newPower > oldMinPower)
                     Main.NewText($"Your Reaver Shark tembles with the power of your fallen foes!\nIt has reached {newPower}% of its potential!", Color.Orange);
@@ -54,42 +57,51 @@ namespace ReviveReaverShark
         /// <returns>The pickaxe power of the world's closest progression.</returns>
         internal static int? GetPower()
         {
-            if (Main.netMode is NetmodeID.Server || Main.gameMenu || WorldGen.gen)
+            if (Main.netMode is NetmodeID.Server || Main.gameMenu || WorldGen.gen || Main.PlayerLoaded is false)
                 return null;
-            if (NPC.downedEmpressOfLight)
-                return 300;
-            else if (NPC.downedMoonlord)
-                return 250;
-            else if (NPC.downedTowerNebula && NPC.downedTowerSolar && NPC.downedTowerStardust && NPC.downedTowerVortex) // Any tower
-                return 225;
-            else if (NPC.downedTowerNebula || NPC.downedTowerSolar || NPC.downedTowerStardust || NPC.downedTowerVortex) // Any tower
-                return 225;
-            else if (NPC.downedAncientCultist)
-                return 220;
-            else if (NPC.downedGolemBoss)
-                return 210;
-            else if (NPC.downedPlantBoss)
-                return 205;
-            else if (NPC.downedMechBoss1 && NPC.downedMechBoss2 && NPC.downedMechBoss3) // All mechs
-                return 200;
-            else if (NPC.downedMechBoss1 || NPC.downedMechBoss2 || NPC.downedMechBoss3) // Any mech
-                return 180;
-            else if (NPC.downedQueenSlime)
-                return 150;
-            else if (Main.hardMode) // WoF
-                return 110;
-            else if (NPC.downedDeerclops) // Skeletron
+
+            if (ServerConfig.dynamicScaling is false)
                 return 100;
-            else if (NPC.downedBoss3) // Skeletron
+
+            if (NPC.downedMoonlord && NPC.downedEmpressOfLight && NPC.downedFishron)
+                return 300;
+            if (NPC.downedMoonlord && NPC.downedFishron)
+                return 285;
+            if (NPC.downedMoonlord || NPC.downedFishron)
+                return 275;
+            if (NPC.downedEmpressOfLight)
+                return 255;
+            if (NPC.downedTowerNebula && NPC.downedTowerSolar && NPC.downedTowerStardust && NPC.downedTowerVortex) // Any tower
+                return 235;
+            if (NPC.downedTowerNebula || NPC.downedTowerSolar || NPC.downedTowerStardust || NPC.downedTowerVortex) // Any tower
+                return 225;
+            if (NPC.downedAncientCultist)
+                return 220;
+            if (NPC.downedGolemBoss)
+                return 210;
+            if (NPC.downedPlantBoss)
+                return 205;
+            if (NPC.downedMechBoss1 && NPC.downedMechBoss2 && NPC.downedMechBoss3) // All mechs
+                return 200;
+            if (NPC.downedMechBoss1 || NPC.downedMechBoss2 || NPC.downedMechBoss3) // Any mech
+                return 180;
+            if (NPC.downedQueenSlime)
+                return 150;
+            if (Main.hardMode) // WoF
+                return 110;
+            if (NPC.downedDeerclops) // Deerclopse
+                return 100;
+            if (NPC.downedBoss3) // Skeletron
                 return 90;
-            else if (NPC.downedQueenBee)
+            if (NPC.downedQueenBee)
                 return 80;
-            else if (NPC.downedBoss2) // Either corruption boss
+            if (NPC.downedBoss2) // Either corruption boss
                 return 70;
-            else if (NPC.downedBoss1) // EoC
-                return 65;
-            else
-                return 59;
+            if (NPC.downedBoss1) // EoC
+                return 64;
+            if (NPC.downedSlimeKing)
+                return 60;
+            return 59;
         }
     }
 
@@ -101,10 +113,12 @@ namespace ReviveReaverShark
         /// <param name="item">The item being being set(up)</param>
         public override void SetDefaults(Item item)
         {
-            base.SetDefaults(item);
             if (item.netID is ItemID.ReaverShark)
                 if (item.active)
-                    item.pick = ReviveReaverShark.GetPower() ?? item.pick;
+                    if (ReviveReaverShark.ServerConfig.dynamicScaling)
+                        item.pick = ReviveReaverShark.GetPower() ?? item.pick;
+                    else
+                        item.pick = 100;
         }
     }
 
@@ -116,7 +130,6 @@ namespace ReviveReaverShark
         /// <param name="player">The player that joined.</param>
         public override void OnEnterWorld(Player player)
         {
-            base.OnEnterWorld(player);
             new Task(delegate
             {
                 Thread.Sleep(1500);
@@ -135,8 +148,6 @@ namespace ReviveReaverShark
         /// <param name="damage">Damage of the hit</param>
         public override void HitEffect(NPC npc, int hitDirection, double damage)
         {
-            base.HitEffect(npc, hitDirection, damage);
-
             if (npc.boss || npc.type is NPCID.LunarTowerVortex or NPCID.LunarTowerStardust or NPCID.LunarTowerSolar or NPCID.LunarTowerNebula) // Is the NPC tied to progression?
                 if (npc.life <= 0) // Is the boss dead/dying?
                     new Task(delegate
